c60fa4ef8303500ba8b104c8482d993cf35bcd2b
Miguel Angel Condori <miguelcondoriwtf2@gmail.com>
2026-06-30 21:21:11 -0400

    optimizacion dp en digitos

--- Problemas/digit dp/Logan and Digit immune numbers.cpp
+++ Problemas/digit dp/Logan and Digit immune numbers.cpp
@@ -1,15 +1,21 @@
 //https://www.codechef.com/problems/DIGIMU
 
+
+//aplicando optimizacion del pref 
 #include <bits/stdc++.h>
 #define int long long
 using namespace std;
 
 const int MOD = 315; 
 
-int memo[20][MOD][2][2][2][2][2][2];
+//[pos][mod][start][h3][h5][h7][h9]
+int memo[20][MOD][2][2][2][2][2];
+int vis[20][MOD][2][2][2][2][2];
+int id = 0;
 
 string num;
 int n;
+
 vector<int> digitos = {0,3,5,7,9};
 
 int dp(int pos, int mod, int pref, int start, int h3, int h5, int h7, int h9) {
@@ -24,32 +30,38 @@ int dp(int pos, int mod, int pref, int start, int h3, int h5, int h7, int h9) {
         return 0;
     }
 
-    int &a = memo[pos][mod][pref][start][h3][h5][h7][h9];
-    if(a == -1){
-        a = 0;
-        int limit = pref? num[pos]-'0':9;
-        int ans = 0;
-        for(auto d: digitos){
-            if(d > limit) continue;
-            int nPref = pref && (d == limit);
-            int nStart = start || (d != 0);
-            if(d == 0 and nStart) continue;
-            int nh3 = h3 || (d == 3);
-            int nh5 = h5 || (d == 5);
-            int nh7 = h7 || (d == 7);
-            int nh9 = h9 || (d == 9);
-            int nMod = nStart? (mod*10+d)%MOD: 0;
-            a += dp(pos+1, nMod, nPref, nStart, nh3, nh5, nh7, nh9);
-        }
+    if(!pref && vis[pos][mod][start][h3][h5][h7][h9] == id){
+        return memo[pos][mod][start][h3][h5][h7][h9];
     }
-    return a;
+
+    
+    int limit = pref? num[pos]-'0':9;
+    int ans = 0;
+    for(auto d: digitos){
+        if(d > limit) continue;
+        int nPref = pref && (d == limit);
+        int nStart = start || (d != 0);
+        if(d == 0 and nStart) continue;
+        int nh3 = h3 || (d == 3);
+        int nh5 = h5 || (d == 5);
+        int nh7 = h7 || (d == 7);
+        int nh9 = h9 || (d == 9);
+        int nMod = nStart? (mod*10+d)%MOD: 0;
+        ans += dp(pos+1, nMod, nPref, nStart, nh3, nh5, nh7, nh9);
+    }
+    
+    if(!pref){
+        vis[pos][mod][start][h3][h5][h7][h9] = id;
+        memo[pos][mod][start][h3][h5][h7][h9] = ans;
+    }
+    return ans;
 }
 
 int f(int x) {
     if (x <= 0) return 0;
     num = to_string(x);
     n = num.size();
-    memset(memo, -1, sizeof memo);
+    id++;
     return dp(0, 0, 1, 0, 0, 0, 0, 0);
 }
 
@@ -80,6 +92,7 @@ void solve(){
 signed main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+    //memset(memo, -1, sizeof memo);
     int t; cin>>t;
      while(t--)
     solve();
--- Problemas/digit dp/The Great Ninja War.cpp
+++ Problemas/digit dp/The Great Ninja War.cpp
@@ -1,19 +1,76 @@
-#include<bits/stdc++.h>
+//https://www.hackerearth.com/problem/algorithm/sallu-bhai-and-ias-8838ac8d/
 
+
+//BUENA PLANTILLA PARA DP DIGIT
+#include<bits/stdc++.h>
+#define int long long
 using namespace std;
 
+const int MOD = 2520;
+
+//[pos][mod][start][mask]
+int memo[15][MOD][2][1024];
+int vis[15][MOD][2][1024];
+int id = 0;
+
+int tabla[10];
+string num;
+int n;
 
 
+int dp(int pos, int sumMod, int pref, int start, int mask) {
+    if (pos == n){
+        if (!start) return 0;
+        for (int d = 1; d <= 9; d++) {
+            if(mask&(1LL<<d)){
+                if(sumMod%d!=0)return 0;
+            }
+        }
+        return 1;
+    }
 
-void solve(){
+    if(!pref && vis[pos][sumMod][start][mask] == id) {
+        return memo[pos][sumMod][start][mask];
+    }
 
+    int ans = 0;
+    int limit = pref ? num[pos]-'0' : 9;
+    for (int d = 0; d <= limit; d++){
+        int nPref = pref && (d == limit);
+        int nStart = start || (d != 0);
+        int nSum = nStart ? (sumMod + (d == 0?0:tabla[d]))%MOD:0;
+        int nMask = (nStart && d != 0) ? (mask|(1 << d)):mask;
+        ans += dp(pos + 1, nSum, nPref, nStart, nMask);
+    }
+
+    if (!pref){
+        vis[pos][sumMod][start][mask] = id;
+        memo[pos][sumMod][start][mask] = ans;
+    }
+    return ans;
+}
+
+long long f(long long x) {
+    if (x <= 0) return 0;
+    num = to_string(x);
+    n = num.size();
+    id++; 
+    return dp(0, 0, 1, 0, 0);
+}
+
+void solve() {
+    int a, b; cin>>a>>b;
+    cout<<f(b)-f(a-1)<<endl;
 }
 
 
 signed main(){
     ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    int t; cin>>t; while(t--)
-    solve();
+    cin.tie(0); cout.tie(0);
+    tabla[0] = 0;
+    for (int i = 1; i <= 9; i++) {
+        tabla[i] = 1;
+        for (int j = 1; j <= i; j++) tabla[i] *= i;
+    }
+    int t; cin>>t; while(t--) solve();
 }
